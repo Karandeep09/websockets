b956e34b8c91aad9ee4a7b443ade8a127137e72c
Karandeep Singh <karandeep.singh@juspay.in>
2023-02-04 10:59:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Server } = require('ws');
+
+const sockserver = new Server({ port: 443 });
+const connections = new Set();
+
+const mp = new Map();
+
+sockserver.on('connection', (ws) => {
+   console.log('New client connected!');
+   connections.add(ws)
+   console.log("Current Clients " + connections.size);
+   ws.on('message', (data) => {
+       data = "" + data
+       if(data.match(/^Hello from/)){
+          mp.set(ws,data.split(/^Hello from */)[1])
+        }
+        console.log(data);
+        console.log(mp);
+       connections.forEach((client) => {
+          if((client != ws) || (data == "Stop"))
+           client.send(data);
+       })
+   });
+
+   ws.on('close', () => {
+       connections.delete(ws);
+       console.log("Current Clients " + connections.size);
+       mp.delete(ws);
+       console.log(mp);
+       console.log('Client has disconnected!');
+   });
+});
